e96adc378f1120201596437476a010b96c1b0f07
MohamedHalim <mohamedhalim1998@gmail.com>
2021-02-15 22:37:07 +0200

    ADD: implement basic navigation

--- app/src/main/java/com/example/moneywellspent/MainActivity.kt
+++ app/src/main/java/com/example/moneywellspent/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.moneywellspent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        val bottomBar = findViewById<BottomAppBar>(R.id.bottom_app_bar)
+        NavigationUI.setupWithNavController(bottomBar, navController)
     }
 }
---
+++ app/src/main/java/com/example/moneywellspent/expenses/ExpensesViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.moneywellspent.expenses
+
+import androidx.lifecycle.ViewModel
+
+class ExpensesViewModel : ViewModel() {
+}
